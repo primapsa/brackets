062041f641b51b5e4417db92c220de15640d4c9e
Siarhei Prymakovich <prima.psa@gmail.com>
2019-03-18 00:23:33 +0300

    feat: add correct bracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ // var bracketsConfig=[['(', ')']];
+  //var str='((()))()))';
+  
+  var res=false;
+  var blacklist=[];
+  var blacklist2=[];
+  var blacklist3=[];
+    if(str.length>0){
+     forwar: for(let i=0; i<str.length; i++){
+       let checker=blacklist.indexOf(i);
+       if(checker==-1){
+        let first=str[i];
+         //console.log('Element: '+first+' num:'+i);
+        for(let t=0; t<bracketsConfig.length; t++){
+          let start=bracketsConfig[t][0];
+          let end=bracketsConfig[t][1];
+          for(let tn=0; tn<bracketsConfig[t].length; tn++){
+            if(first===start){
+              //console.log('Find: '+start+' num:'+i);
+              let second=str[i+1];
+              //console.log('Search: '+end+' num: '+i);
+              
+              for(let j=i; j<str.length; j++){
+                
+                //console.log('test: '+j);
+                if(blacklist.indexOf(j)==-1){
+                //  console.log('test2: '+j);
+                  // console.log('test___: '+str[j]);
+                if(end===str[j]){
+                 // console.log('test3: '+j);
+                 // console.log('End: '+end+' num: '+j);
+                 if(j!==i+2){
+                  let addtolist=blacklist.push(i,j);
+                 }
+                  //let addtolist=blacklist3.push(i,j);
+                  //console.log('-----'+blacklist3+'------');
+                  continue forwar;
+                }
+                }
+              
+              }
+            }
+          }
+        }
+      }
+      }
+      }
+  if(str.length-blacklist.length==0){
+    res=true;
+  }else{
+    res=false;
+  }
+  return res;
+  
+  
+  
+  
+  
+  
 }
