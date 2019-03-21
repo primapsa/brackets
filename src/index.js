c5f2ed20e2c1342e703ad31f1f9406ad339f5b21
Siarhei Prymakovich <prima.psa@gmail.com>
2019-03-21 19:20:32 +0300

    feat: finish brackets function

--- src/index.js
+++ src/index.js
@@ -2,57 +2,50 @@ module.exports = function check(str, bracketsConfig) {
   var check=[],
   checkStart,
   checkEnd,
-  compare
-result=true;
-
-
-
-function search(bracket='', brackets=[], open=true) {
-let position=false; 
-let bracketStart=0;
-if(!open){
-  bracketStart=1;
-}
- for(let s=0;s<brackets.length; s++){
-   
-     if(brackets[s][bracketStart]===bracket){
-       position=s;
-       break;
-     
-   }    
- }
-return position;
-}
-
-
-
-for(let i=0; i<str.length; i++){
-checkStart=search(str[i], bracketsConfig, true);
-//console.log('4--'+checkStart);
-if(checkStart!==false){
-  check.push(checkStart);
- // console.log(check);
-  continue;    
-}
- checkEnd=search(str[i], bracketsConfig, false);
- //console.log('3--'+checkEnd);
-if(checkEnd!==false){
-  compare=check.pop();
- // console.log('2--'+check);
-  if(compare!==checkEnd){
-    result=false;
-    break;
+  compare,
+  result=true;
+    function search(bracket='', brackets=[], open=true) {
+      let position=false; 
+      let bracketStart=0;
+        if(!open){
+          bracketStart=1;
+        }
+        for(let s=0;s<brackets.length; s++){   
+            if(brackets[s][bracketStart]===bracket){
+              position=s;
+              break;     
+          }    
+        }
+      return position;
+    }
+  
+for(let i=0; i<str.length; i++){  
+  checkStart=search(str[i], bracketsConfig, true);
+  if(checkStart!==false){  
+    if(bracketsConfig[checkStart][0]===bracketsConfig[checkStart][1]) { 
+      if(check.indexOf(checkStart)==-1){
+        check.push(checkStart);
+        continue; 
+        }
+    }
+    else{
+      check.push(checkStart);
+      continue; 
+    }   
   }
   
-}
+  checkEnd=search(str[i], bracketsConfig, false); 
+  if(checkEnd!==false){
+    compare=check.pop(); 
+    if(compare!==checkEnd){
+      result=false;
+      break;
+    }  
+  }
 
 }
-if (check.length !== 0) {
-result=false;
-if(check.length%2==0){
-  result=true;   
-}
-}
-return result;
-
+  if (check.length !== 0) {
+  result=false;
+  }
+  return result;
 }
